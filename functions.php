401edf38f2a76687f40bdec4a34157895d320bc2
Alexander Osadchyy <deluxart3@gmail.com>
2019-09-28 19:42:46 +0300

    fix

--- functions.php
+++ functions.php
@@ -266,4 +266,4 @@ add_theme_support ( 'genesis-menus' , array (
 
 
 // Bring in my custom walker class - filed in my themes inc/
-require_once dirname( __FILE__ ) . '/inc/extend-walker-class.php';
+require_once dirname( __FILE__ ) . '/inc/menu-items.php';
