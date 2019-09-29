c3e17a4137c8fe4fae8dbd59b3cdcf7b7172f78b
Alexander Osadchyy <deluxart3@gmail.com>
2019-09-29 19:26:51 +0300

    fix

--- inc/options_page.php
+++ inc/options_page.php
@@ -7,14 +7,14 @@ register_setting( 'dart_options', 'dart_theme_options');
 }
 
 function theme_options_add_page() {
-add_menu_page( __( 'Основные настройки', 'WP-Ember' ), __( 'Основные настройки', 'WP-Ember' ), 'edit_theme_options', 'price_options', 'theme_options_do_page', 'dashicons-editor-kitchensink', 4 );
+add_menu_page( __( 'Базовые', 'WP-Ember' ), __( 'Базовые', 'WP-Ember' ), 'edit_theme_options', 'price_options', 'theme_options_do_page', 'dashicons-editor-kitchensink', 4 );
 }
 function theme_options_do_page() { global $select_options; if ( ! isset( $_REQUEST['settings-updated'] ) ) $_REQUEST['settings-updated'] = false;
     // here we adding our custom meta box
 ?>
 
 <div class="wrap">
-<?php screen_icon(); echo "<h2>". __( 'Создать халат - настройка цен', 'WP-Ember' ) . "</h2>"; ?>
+<?php screen_icon(); echo "<h2>". __( 'Базовые настройки', 'WP-Ember' ) . "</h2>"; ?>
 
     <?php if (!empty($notice)): ?>
     <div id="notice" class="error"><p><?php echo $notice ?></p></div>
@@ -50,10 +50,21 @@ function theme_options_do_page() { global $select_options; if ( ! isset( $_REQUE
 
     <tr class="form-field">
         <th valign="top" scope="row">
+            <label>Ссылка на <strong>Behance</strong></label>
+        </th>
+        <td>
+           <input id="dart_theme_options[be]" name="dart_theme_options[be]" type="text" style="width: 100%" value="<?php echo $options[be];?>" size="50" class="regular-text" placeholder="Введите ссылку на Behance" >
+        </td>
+    </tr>
+
+
+
+    <tr class="form-field">
+        <th valign="top" scope="row">
             <label>Ссылка на <strong>Facebook</strong></label>
         </th>
         <td>
-           <input id="dart_theme_options[fb]" name="dart_theme_options[fb]" type="text" style="width: 95%" value="<?php echo $options[fb];?>" size="50" class="regular-text" placeholder="Введите ссылку на Facebook" >
+           <input id="dart_theme_options[fb]" name="dart_theme_options[fb]" type="text" style="width: 100%" value="<?php echo $options[fb];?>" size="50" class="regular-text" placeholder="Введите ссылку на Facebook" >
         </td>
     </tr>
 
@@ -63,7 +74,16 @@ function theme_options_do_page() { global $select_options; if ( ! isset( $_REQUE
             <label>Ссылка на <strong>Instagram</strong></label>
         </th>
         <td>
-           <input id="dart_theme_options[in]" name="dart_theme_options[in]" type="text" style="width: 95%" value="<?php echo $options[in];?>" size="50" class="regular-text" placeholder="Введите ссылку на Instagram" required="">
+           <input id="dart_theme_options[in]" name="dart_theme_options[in]" type="text" style="width: 100%" value="<?php echo $options[in];?>" size="50" class="regular-text" placeholder="Введите ссылку на Instagram" required="">
+        </td>
+    </tr>
+
+    <tr class="form-field">
+        <th valign="top" scope="row">
+            <label>Ссылка на <strong>Telegram</strong></label>
+        </th>
+        <td>
+           <input id="dart_theme_options[tg]" name="dart_theme_options[tg]" type="text" style="width: 100%" value="<?php echo $options[tg];?>" size="50" class="regular-text" placeholder="Введите ссылку на Telegram" required="">
         </td>
     </tr>
 
@@ -73,7 +93,7 @@ function theme_options_do_page() { global $select_options; if ( ! isset( $_REQUE
             <label>Номер телефона</label>
         </th>
         <td>
-           <input id="dart_theme_options[phone_1]" name="dart_theme_options[phone_1]" type="text" style="width: 95%" value="<?php echo $options[phone_1];?>" size="50" class="regular-text" placeholder="Введите основной номер телефона">
+           <input id="dart_theme_options[phone_1]" name="dart_theme_options[phone_1]" type="text" style="width: 100%" value="<?php echo $options[phone_1];?>" size="50" class="regular-text" placeholder="Введите основной номер телефона">
         </td>
     </tr>
 
@@ -82,7 +102,7 @@ function theme_options_do_page() { global $select_options; if ( ! isset( $_REQUE
             <label>Дополнительный номер телефона</label>
         </th>
         <td>
-           <input id="dart_theme_options[phone_2]" name="dart_theme_options[phone_2]" type="text" style="width: 95%" value="<?php echo $options[phone_2];?>" size="50" class="regular-text" placeholder="Введите дополнительный номер телефона">
+           <input id="dart_theme_options[phone_2]" name="dart_theme_options[phone_2]" type="text" style="width: 100%" value="<?php echo $options[phone_2];?>" size="50" class="regular-text" placeholder="Введите дополнительный номер телефона">
         </td>
     </tr>
 
@@ -93,7 +113,7 @@ function theme_options_do_page() { global $select_options; if ( ! isset( $_REQUE
             <label>Copyright:</label>
         </th>
         <td>
-           <input id="dart_theme_options[copyright]" name="dart_theme_options[copyright]" type="text" style="width: 95%" value="<?php echo $options[copyright];?>" size="50" class="regular-text" placeholder="Введите текст для copyright">
+           <input id="dart_theme_options[copyright]" name="dart_theme_options[copyright]" type="text" style="width: 100%" value="<?php echo $options[copyright];?>" size="50" class="regular-text" placeholder="Введите текст для copyright">
         </td>
     </tr>
 
