62e52168cc190968efa27814096df639e324acb4
Alexander Osadchyy <deluxart3@gmail.com>
2019-09-30 22:04:38 +0300

    fix

--- functions.php
+++ functions.php
@@ -268,7 +268,17 @@ function my_shortcode_function() {
 	posts_nav_link(); // пагинация - echo тут не надо
 	wp_reset_query(); // сброс $wp_query
 
-
+?>
+<?php if (  $wp_query->max_num_pages > 1 ) : ?>
+	<script>
+	var ajaxurl = '<?php echo site_url() ?>/wp-admin/admin-ajax.php';
+	var true_posts = '<?php echo serialize($wp_query->query_vars); ?>';
+	var current_page = <?php echo (get_query_var('paged')) ? get_query_var('paged') : 1; ?>;
+	var max_pages = '<?php echo $wp_query->max_num_pages; ?>';
+	</script>
+	<div id="true_loadmore">Загрузить ещё</div>
+<?php endif; ?>
+<?php
 // Рабочий вариант
 // global $wp_query;
 // $wp_query = new WP_Query(array(
