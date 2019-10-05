9e2b3c39f171b92431f7c55f1a0edcbff103c4f1
Alexander Osadchyy <deluxart3@gmail.com>
2019-10-05 22:37:23 +0300

    fix

--- template-parts/portfolio.php
+++ template-parts/portfolio.php
@@ -14,7 +14,7 @@
 
 <div class="wow slideInRight" data-wow-duration="2s" data-wow-delay="0.2s" id="post-<?php the_ID(); ?>">
     <a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title(); ?>">
-    <?php if ( get_field( 'prevyu' ) ) { ?><img src="<?php the_field( 'prevyu' ); ?>" /><?php } ?>
+    <?php if ( get_field( 'prevyu' ) ) { ?><img src="<?php the_field( 'prevyu' ); ?>" alt="<?php the_title(); ?>" /><?php } ?>
 		<div class="datails">
 			<div>
 				<h4><?php the_title(); ?></h4>
@@ -32,10 +32,5 @@ if ( $metki_array ):
     endforeach;
 echo '</ul>';
 endif; ?>
-<!--
-		<ul>
-			<li><a href="#">Веб-дизайн</a></li>
-			<li><a href="#">Верстка</a></li>
-		</ul> -->
 	</a>
 </div><!-- #portfolio-<?php the_ID(); ?> -->
