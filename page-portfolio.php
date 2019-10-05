6d129ae7aa313650a249959a8c8700d4496b18d2
Alexander Osadchyy <deluxart3@gmail.com>
2019-10-05 21:48:51 +0300

    fix

--- page-portfolio.php
+++ page-portfolio.php
@@ -15,24 +15,19 @@
 get_header();
 ?>
 
-	<div id="primary" class="content-area testeddddd">
-		<main id="main" class="site-main">
+	<div id="portfolio" class="content-area">
+		<div class="container">
+            <div class="portfolio">
+                <?php
+                while ( have_posts() ) :
+                    the_post();
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+                    get_template_part( 'template-parts/content', 'page' );
+                endwhile; // End of the loop.
+                ?>
 
-			get_template_part( 'template-parts/content', 'page' );
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
-
-		</main><!-- #main -->
+	            </div>
+        </div><!-- #main -->
 	</div><!-- #primary -->
 
 <?php
