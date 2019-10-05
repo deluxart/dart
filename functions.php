e3d19c9becb77c1c2dcdb3ad635ce9ef35b512b7
Alexander Osadchyy <deluxart3@gmail.com>
2019-10-05 16:10:00 +0300

    reg type portfolio

--- functions.php
+++ functions.php
@@ -347,3 +347,44 @@ function list_menu($atts, $content = null) {
 }
 //Create the shortcode
 add_shortcode("listmenu", "list_menu");
+
+
+
+
+
+
+
+
+
+
+add_action('init', 'my_custom_init');
+function my_custom_init(){
+	register_post_type('portfolio', array(
+		'labels'             => array(
+			'name'               => 'Портфолио', // Основное название типа записи
+			'singular_name'      => 'Портфолио', // отдельное название записи типа Book
+			'add_new'            => 'Добавить новую',
+			'add_new_item'       => 'Добавить новую работу',
+			'edit_item'          => 'Редактировать работу',
+			'new_item'           => 'Новая работа',
+			'view_item'          => 'Посмотреть работу',
+			'search_items'       => 'Найти работу',
+			'not_found'          =>  'Работ не найдено',
+			'not_found_in_trash' => 'В корзине работ не найдено',
+			'parent_item_colon'  => '',
+			'menu_name'          => 'Портфолио'
+
+		  ),
+		'public'             => true,
+		'publicly_queryable' => true,
+		'show_ui'            => true,
+		'show_in_menu'       => true,
+		'query_var'          => true,
+		'rewrite'            => true,
+		'capability_type'    => 'post',
+		'has_archive'        => true,
+		'hierarchical'       => false,
+		'menu_position'      => null,
+		'supports'           => array('title','editor','author','thumbnail','excerpt','comments')
+	) );
+}
