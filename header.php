838c81896b55c5277ac267259345b28ff0374772
Alexander Osadchyy <deluxart3@gmail.com>
2019-10-05 22:47:04 +0300

    fix_last_bug (by wc3 validator)

--- header.php
+++ header.php
@@ -16,7 +16,7 @@
 <html <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, maximum-scale=1.0,  user-scalable=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
     <link rel="icon" type="image/x-icon" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon.png">
