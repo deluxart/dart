ca9a8bfd11458e1f79b419ead2b2d41173600a0e
Alexander Osadchyy <deluxart3@gmail.com>
2019-10-05 21:49:52 +0300

    fix

--- page-portfolio.php
+++ page-portfolio.php
@@ -22,7 +22,7 @@ get_header();
                 while ( have_posts() ) :
                     the_post();
 
-                    get_template_part( 'template-parts/content', 'page' );
+                    get_template_part( 'template-parts/portfolio', 'page' );
                 endwhile; // End of the loop.
                 ?>
 
