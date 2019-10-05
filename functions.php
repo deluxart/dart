3605eb6cc85bc6fa978a184a4a6c80e64a2550f1
Alexander Osadchyy <deluxart3@gmail.com>
2019-10-05 19:26:21 +0300

    fix

--- functions.php
+++ functions.php
@@ -439,3 +439,24 @@ echo '</div>';
 add_filter( 'get_the_archive_title', function( $title ){
 	return preg_replace('~^[^:]+: ~', '', $title );
 });
+
+
+add_filter( 'get_the_archive_title', function ($title) {
+
+    if ( is_category() ) {
+
+            $title = single_cat_title( '', false );
+
+        } elseif ( is_tag() ) {
+
+            $title = single_tag_title( '', false );
+
+        } elseif ( is_author() ) {
+
+            $title = '<span class="vcard">' . get_the_author() . '</span>' ;
+
+        }
+
+    return $title;
+
+});
