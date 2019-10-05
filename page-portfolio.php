7426ff7941d101d800befbc3750eee9aadfd258a
Alexander Osadchyy <deluxart3@gmail.com>
2019-10-05 21:56:10 +0300

    fix

--- page-portfolio.php
+++ page-portfolio.php
@@ -19,14 +19,22 @@ get_header();
 		<div class="container">
             <div class="portfolio">
 <?php
+if ( get_query_var('paged') ) $paged = get_query_var('paged');
+if ( get_query_var('page') ) $paged = get_query_var('page');
 
-  $args = array('post_type'=>array('posts', 'portfolio'));
+$query = new WP_Query( array( 'post_type' => 'portfolio', 'paged' => $paged ) );
 
-  query_posts($args);
-
-  if ( have_posts() ) : while ( have_posts() ) : the_post();
-
-?>
+if ( $query->have_posts() ) : ?>
+	<?php while ( $query->have_posts() ) : $query->the_post(); ?>
+		<div class="entry">
+			<h2 class="title"><?php the_title(); ?></h2>
+			<?php the_content(); ?>
+		</div>
+	<?php endwhile; wp_reset_postdata(); ?>
+	<!-- show pagination here -->
+<?php else : ?>
+	<!-- show 404 error here -->
+<?php endif; ?>
 
 	            </div>
         </div><!-- #main -->
