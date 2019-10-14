e2167db8ac879783432cc949033e4e91c2b9dc23
Alexander Osadchyy <deluxart3@gmail.com>
2019-10-14 17:10:48 +0300

    fix

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -11,7 +11,9 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+        <div class="container">
+		    <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+        </div>
 	</header><!-- .entry-header -->
 
 	<?php delux_art_post_thumbnail(); ?>
