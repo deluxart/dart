489adc2c76c2a2ca5b832114e1e8893b37126eaa
Alexander Osadchyy <deluxart3@gmail.com>
2019-10-14 19:50:36 +0300

    fix

--- 404.php
+++ 404.php
@@ -14,10 +14,10 @@ get_header();
 		<main id="main" class="site-main">
 
 			<section class="error-404 not-found">
+                <div class="container">
 				<header class="page-header">
 					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'delux-art' ); ?></h1>
 				</header><!-- .page-header -->
-                <div class="container">
 				<div class="page-content">
 					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'delux-art' ); ?></p>
 
