d79bc373304acdf7ecc1a9bb613718fc7c358358
Alexander Osadchyy <deluxart3@gmail.com>
2019-10-24 21:05:58 +0300

    fix

--- inc/options_page.php
+++ inc/options_page.php
@@ -7,14 +7,14 @@ register_setting( 'dart_options', 'dart_theme_options');
 }
 
 function theme_options_add_page() {
-add_menu_page( __( 'Базовые', 'WP-Ember' ), __( 'Базовые', 'WP-Ember' ), 'edit_theme_options', 'price_options', 'theme_options_do_page', 'dashicons-editor-kitchensink', 4 );
+add_menu_page( __( 'Базовые', 'WP-Dart' ), __( 'Базовые', 'WP-Dart' ), 'edit_theme_options', 'price_options', 'theme_options_do_page', 'dashicons-editor-kitchensink', 4 );
 }
 function theme_options_do_page() { global $select_options; if ( ! isset( $_REQUEST['settings-updated'] ) ) $_REQUEST['settings-updated'] = false;
     // here we adding our custom meta box
 ?>
 
 <div class="wrap">
-<?php screen_icon(); echo "<h2>". __( 'Базовые настройки', 'WP-Ember' ) . "</h2>"; ?>
+<?php screen_icon(); echo "<h2>". __( 'Базовые настройки', 'WP-Dart' ) . "</h2>"; ?>
 
     <?php if (!empty($notice)): ?>
     <div id="notice" class="error"><p><?php echo $notice ?></p></div>
@@ -27,7 +27,7 @@ function theme_options_do_page() { global $select_options; if ( ! isset( $_REQUE
 
 <?php if ( false !== $_REQUEST['settings-updated'] ) : ?>
 <div id="message" class="updated">
-<p><strong><?php _e( 'Настройки сохранены', 'WP-Ember' ); ?></strong></p>
+<p><strong><?php _e( 'Настройки сохранены', 'WP-Dart' ); ?></strong></p>
 </div>
 <?php endif; ?>
 -->
@@ -53,7 +53,7 @@ function theme_options_do_page() { global $select_options; if ( ! isset( $_REQUE
             <label>Ссылка на <strong>Behance</strong></label>
         </th>
         <td>
-           <input id="dart_theme_options[be]" name="dart_theme_options[be]" type="text" style="width: 100%" value="<?php echo $options[be];?>" size="50" class="regular-text" placeholder="Введите ссылку на Behance" >
+           <input id="dart_theme_options[be]" name="dart_theme_options[be]" type="text" style="width: 100%" value="<?php echo $options['be'];?>" size="50" class="regular-text" placeholder="Введите ссылку на Behance" >
         </td>
     </tr>
 
@@ -64,7 +64,7 @@ function theme_options_do_page() { global $select_options; if ( ! isset( $_REQUE
             <label>Ссылка на <strong>Facebook</strong></label>
         </th>
         <td>
-           <input id="dart_theme_options[fb]" name="dart_theme_options[fb]" type="text" style="width: 100%" value="<?php echo $options[fb];?>" size="50" class="regular-text" placeholder="Введите ссылку на Facebook" >
+           <input id="dart_theme_options[fb]" name="dart_theme_options[fb]" type="text" style="width: 100%" value="<?php echo $options['fb'];?>" size="50" class="regular-text" placeholder="Введите ссылку на Facebook" >
         </td>
     </tr>
 
@@ -74,7 +74,7 @@ function theme_options_do_page() { global $select_options; if ( ! isset( $_REQUE
             <label>Ссылка на <strong>Instagram</strong></label>
         </th>
         <td>
-           <input id="dart_theme_options[in]" name="dart_theme_options[in]" type="text" style="width: 100%" value="<?php echo $options[in];?>" size="50" class="regular-text" placeholder="Введите ссылку на Instagram" required="">
+           <input id="dart_theme_options[in]" name="dart_theme_options[in]" type="text" style="width: 100%" value="<?php echo $options['in'];?>" size="50" class="regular-text" placeholder="Введите ссылку на Instagram" required="">
         </td>
     </tr>
 
@@ -83,7 +83,7 @@ function theme_options_do_page() { global $select_options; if ( ! isset( $_REQUE
             <label>Ссылка на <strong>Telegram</strong></label>
         </th>
         <td>
-           <input id="dart_theme_options[tg]" name="dart_theme_options[tg]" type="text" style="width: 100%" value="<?php echo $options[tg];?>" size="50" class="regular-text" placeholder="Введите ссылку на Telegram" required="">
+           <input id="dart_theme_options[tg]" name="dart_theme_options[tg]" type="text" style="width: 100%" value="<?php echo $options['tg'];?>" size="50" class="regular-text" placeholder="Введите ссылку на Telegram" required="">
         </td>
     </tr>
 
@@ -93,7 +93,7 @@ function theme_options_do_page() { global $select_options; if ( ! isset( $_REQUE
             <label>Номер телефона</label>
         </th>
         <td>
-           <input id="dart_theme_options[phone_1]" name="dart_theme_options[phone_1]" type="text" style="width: 100%" value="<?php echo $options[phone_1];?>" size="50" class="regular-text" placeholder="Введите основной номер телефона">
+           <input id="dart_theme_options[phone_1]" name="dart_theme_options[phone_1]" type="text" style="width: 100%" value="<?php echo $options['phone_1'];?>" size="50" class="regular-text" placeholder="Введите основной номер телефона">
         </td>
     </tr>
 
@@ -102,7 +102,7 @@ function theme_options_do_page() { global $select_options; if ( ! isset( $_REQUE
             <label>Дополнительный номер телефона</label>
         </th>
         <td>
-           <input id="dart_theme_options[phone_2]" name="dart_theme_options[phone_2]" type="text" style="width: 100%" value="<?php echo $options[phone_2];?>" size="50" class="regular-text" placeholder="Введите дополнительный номер телефона">
+           <input id="dart_theme_options[phone_2]" name="dart_theme_options[phone_2]" type="text" style="width: 100%" value="<?php echo $options['phone_2'];?>" size="50" class="regular-text" placeholder="Введите дополнительный номер телефона">
         </td>
     </tr>
 
@@ -113,7 +113,7 @@ function theme_options_do_page() { global $select_options; if ( ! isset( $_REQUE
             <label>Email:</label>
         </th>
         <td>
-           <input id="dart_theme_options[email]" name="dart_theme_options[email]" type="text" style="width: 100%" value="<?php echo $options[email];?>" size="50" class="regular-text" placeholder="Введите текст для email">
+           <input id="dart_theme_options[email]" name="dart_theme_options[email]" type="text" style="width: 100%" value="<?php echo $options['email'];?>" size="50" class="regular-text" placeholder="Введите текст для email">
         </td>
     </tr>
 
