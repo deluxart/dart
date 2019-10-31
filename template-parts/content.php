e0bb177483bf80ca49c8fb13c75dd51e2712612c
Alexander Osadchyy <deluxart3@gmail.com>
2019-10-31 20:59:26 +0200

    fix

--- template-parts/content.php
+++ template-parts/content.php
@@ -10,7 +10,7 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
+	<header class="entry-header tteesstt">
         <div class="container">
 		<?php
 		if ( is_singular() ) :
