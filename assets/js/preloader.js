e21a510328aedc6e81ec86d3618f6106f1a0adbd
Alexander Osadchyy <deluxart3@gmail.com>
2019-11-11 23:22:48 +0200

    fix

--- assets/js/preloader.js
+++ assets/js/preloader.js
@@ -36,8 +36,7 @@ jQuery(document).ready(function() {
     if (preloader.enabled) {
 		(function() {
 			var block = jQuery('.b-preloader');
-            var items = block.find('.b-preloader-item');
-            var items_last = block.find('.b-preloader-item.last');
+			var items = block.find('.b-preloader-item');
 			function preloaderSetItem(n) {
 				setTimeout(function() {
 					items.eq(n).addClass('show').prev().removeClass('show');
@@ -64,8 +63,7 @@ jQuery(document).ready(function() {
                 block.addClass('bg-black show');
                 jQuery('body').addClass('preloader');
 				setTimeout(function() {
-                    items.not(':last-child').addClass('zoom-out');
-                    items_last.addClass('zoom-in');
+					items.not(':last-child').addClass('zoom-out');
 					for (i = 0; i < items.length; i++) {
 						preloaderSetItem(i);
 					}
