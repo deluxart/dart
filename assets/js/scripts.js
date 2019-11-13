ce1ce27bbd6f9f4b05778ad5143d7207dfc3d4fc
Alexander Osadchyy <deluxart3@gmail.com>
2019-11-13 21:40:07 +0200

    fix

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -9,39 +9,14 @@
 
 
 var deluxart = {
-    cursorPosition: {
-        x: Math.ceil(jQuery(window).width() / 2),
-        y: Math.ceil(jQuery(window).height() / 2)
-    },
-    isMobile: (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)),
-    isTouch: Modernizr.touchevents,
-    scrollWidth: window.innerWidth - document.documentElement.clientWidth,
-    sliders: {
-        homescreenCases: { // DEPRECATED
-            current: 0,
-            offset: 0,
-            slideDelta: 300,
-            updateInfo: function () {
-                var slide = jQuery('.b-main-cases-slide.active');
-                var info = jQuery('.b-main-cases-slide-content');
-                //var logo = info.find('.b-main-cases-slide-logo');
-                info.addClass('animate');
-                setTimeout(function () {
-                    //logo.empty();
-                    //slide.find('.b-main-cases-slide-logo img').clone().appendTo(logo);
-                    info.find('.b-main-cases-slide-title').text(slide.data('title'));
-                    info.find('.b-main-cases-slide-text').text(slide.data('text'));
-                    info.attr('href', slide.attr('href'));
-                }, 450);
-                setTimeout(function () {
-                    info.removeClass('animate');
-                }, 900);
-            }
-        },
-        portfolio: {
+    // cursorPosition: {
+    //     x: Math.ceil(jQuery(window).width() / 2),
+    //     y: Math.ceil(jQuery(window).height() / 2)
+    // },
+    // isMobile: (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)),
+    // isTouch: Modernizr.touchevents,
+    // scrollWidth: window.innerWidth - document.documentElement.clientWidth,
 
-        }
-    },
     functions: {
         cookieSet: function (name, value, options) {
             options = options || {};
@@ -74,80 +49,80 @@ var deluxart = {
             var matches = document.cookie.match(new RegExp("(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"));
             return matches ? decodeURIComponent(matches[1]) : undefined;
         },
-        scrollLock: function () {
-            jQuery('html').addClass('scroll-lock');
-            jQuery('.main-wrapper, .b-fixed-content, .b-header').css('padding-right', deluxart.scrollWidth + 'px');
-        },
-        scrollUnlock: function () {
-            jQuery('html').removeClass('scroll-lock');
-            jQuery('.main-wrapper, .b-fixed-content, .b-header').css('padding-right', '0');
-        },
-        scrollPageTo: function (anchor) {
-            var target = jQuery(anchor);
-            if (target.length) {
-                jQuery('html, body').animate({
-                    scrollTop: target.offset().top
-                }, 1500);
-                jQuery('body').removeClass('menu-active');
-            }
-        },
-        getCursorCenterOffset: function () {
-            var windowCenterX = jQuery(window).width() / 2;
-            var windowCenterY = jQuery(window).height() / 2;
-            return {
-                x: Math.ceil(deluxart.cursorPosition.x - windowCenterX),
-                y: Math.ceil(deluxart.cursorPosition.y - windowCenterY)
-            };
-        },
-        getBlockScrollProgress: function (block, bottom) {
-            var result = 0;
-            var windowScrollPosition = jQuery(window).scrollTop();
-            var blockOffsetTop = block.offset().top;
-            var blockHeight = block.outerHeight();
-            var blockOffsetBottom = blockOffsetTop + blockHeight;
-            if (bottom)
-                windowScrollPosition += jQuery(window).outerHeight();
-            if (windowScrollPosition >= blockOffsetTop && windowScrollPosition <= blockOffsetBottom)
-                result = Math.ceil(((windowScrollPosition - blockOffsetTop) / blockHeight) * 100);
-            if (windowScrollPosition > blockOffsetBottom)
-                result = 100;
-            return result;
-        },
-        getBlockViewportProgress: function (block) {
-            var windowScrollPositionTop = jQuery(window).scrollTop();
-            var windowScrollPositionBottom = windowScrollPositionTop + jQuery(window).outerHeight();
-            var blockOffsetTop = block.offset().top;
-            var blockOffsetBottom = blockOffsetTop + block.outerHeight();
-            if (this.checkBlockIsInViewport(block))
-                return Math.ceil((Math.ceil((blockOffsetBottom + blockOffsetTop) / 2) - windowScrollPositionTop) / (windowScrollPositionBottom - windowScrollPositionTop) * 100);
-            else
-                return 0;
-        },
-        checkBlockIsInViewport: function (block) {
-            if (block.length)
-                return (this.getBlockScrollProgress(block, true) > 0 && this.getBlockScrollProgress(block) < 100);
-            else
-                return false;
-        },
-        addZeroes: function (number, length) {
-            var result = '' + number;
-            while (result.length < length)
-                result = '0' + result;
-            return result;
-        },
-        fixedPlaceholder: function () {
-            jQuery('.b-fixed').each(function () {
-                var content = jQuery(this).find('.b-fixed-content');
-                jQuery(this).css('height', content.height() + 'px');
-            });
-        },
-        portfolioFixUp: function () {
-            var arrow = jQuery('.b-portfolio-up');
-            var firstItem = jQuery('.b-portfolio-item-image:visible').first();
-            if (firstItem.length) {
-                arrow.css('top', firstItem.height() + 60 + 'px');
-            }
-        }
+        // scrollLock: function () {
+        //     jQuery('html').addClass('scroll-lock');
+        //     jQuery('.main-wrapper, .b-fixed-content, .b-header').css('padding-right', deluxart.scrollWidth + 'px');
+        // },
+        // scrollUnlock: function () {
+        //     jQuery('html').removeClass('scroll-lock');
+        //     jQuery('.main-wrapper, .b-fixed-content, .b-header').css('padding-right', '0');
+        // },
+        // scrollPageTo: function (anchor) {
+        //     var target = jQuery(anchor);
+        //     if (target.length) {
+        //         jQuery('html, body').animate({
+        //             scrollTop: target.offset().top
+        //         }, 1500);
+        //         jQuery('body').removeClass('menu-active');
+        //     }
+        // },
+        // getCursorCenterOffset: function () {
+        //     var windowCenterX = jQuery(window).width() / 2;
+        //     var windowCenterY = jQuery(window).height() / 2;
+        //     return {
+        //         x: Math.ceil(deluxart.cursorPosition.x - windowCenterX),
+        //         y: Math.ceil(deluxart.cursorPosition.y - windowCenterY)
+        //     };
+        // },
+        // getBlockScrollProgress: function (block, bottom) {
+        //     var result = 0;
+        //     var windowScrollPosition = jQuery(window).scrollTop();
+        //     var blockOffsetTop = block.offset().top;
+        //     var blockHeight = block.outerHeight();
+        //     var blockOffsetBottom = blockOffsetTop + blockHeight;
+        //     if (bottom)
+        //         windowScrollPosition += jQuery(window).outerHeight();
+        //     if (windowScrollPosition >= blockOffsetTop && windowScrollPosition <= blockOffsetBottom)
+        //         result = Math.ceil(((windowScrollPosition - blockOffsetTop) / blockHeight) * 100);
+        //     if (windowScrollPosition > blockOffsetBottom)
+        //         result = 100;
+        //     return result;
+        // },
+        // getBlockViewportProgress: function (block) {
+        //     var windowScrollPositionTop = jQuery(window).scrollTop();
+        //     var windowScrollPositionBottom = windowScrollPositionTop + jQuery(window).outerHeight();
+        //     var blockOffsetTop = block.offset().top;
+        //     var blockOffsetBottom = blockOffsetTop + block.outerHeight();
+        //     if (this.checkBlockIsInViewport(block))
+        //         return Math.ceil((Math.ceil((blockOffsetBottom + blockOffsetTop) / 2) - windowScrollPositionTop) / (windowScrollPositionBottom - windowScrollPositionTop) * 100);
+        //     else
+        //         return 0;
+        // },
+        // checkBlockIsInViewport: function (block) {
+        //     if (block.length)
+        //         return (this.getBlockScrollProgress(block, true) > 0 && this.getBlockScrollProgress(block) < 100);
+        //     else
+        //         return false;
+        // },
+        // addZeroes: function (number, length) {
+        //     var result = '' + number;
+        //     while (result.length < length)
+        //         result = '0' + result;
+        //     return result;
+        // },
+        // fixedPlaceholder: function () {
+        //     jQuery('.b-fixed').each(function () {
+        //         var content = jQuery(this).find('.b-fixed-content');
+        //         jQuery(this).css('height', content.height() + 'px');
+        //     });
+        // },
+        // portfolioFixUp: function () {
+        //     var arrow = jQuery('.b-portfolio-up');
+        //     var firstItem = jQuery('.b-portfolio-item-image:visible').first();
+        //     if (firstItem.length) {
+        //         arrow.css('top', firstItem.height() + 60 + 'px');
+        //     }
+        // }
     }
 };
 
