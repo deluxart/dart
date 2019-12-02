b3db3aab54058d1577725dcf6f51e62d82e57366
Alexander Osadchyy <deluxart3@gmail.com>
2019-12-02 21:47:25 +0200

    fix

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -172,6 +172,8 @@ jQuery(document).ready(function () {
 jQuery('.home_slider_slick').slick({
     dots: true,
     arrows: true,
+    prevArrow: "<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+    nextArrow: "<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
     infinite: true,
     speed: 300,
     slidesToShow: 1,
