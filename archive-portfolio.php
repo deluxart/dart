85f7281c22e3f3e9dfa35621f9c08e5d74dadc41
Alexander Osadchyy <deluxart3@gmail.com>
2020-01-26 20:02:18 +0200

    fix

--- archive-portfolio.php
+++ archive-portfolio.php
@@ -35,7 +35,7 @@ $page_description = get_field( 'page_description' );
 				 * If you want to override this in a child theme, then include a file
 				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
 				 */
-				get_template_part( 'template-parts/content', get_post_type() );
+                get_template_part( 'template-parts/portfolio', get_post_format() );
 
 			endwhile;
 
